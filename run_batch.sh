5f57a5e403fcaa2298329a889bf36528ebda609a
Kevin Zhang <kevinz@layer6.ai>
2025-09-12 14:05:46 -0400

    batch run

---
+++ run_batch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+for i in {1..10}
+do
+    nohup python -u run.py \
+        --test_file ./data/subset50.jsonl \
+        --headless \
+        --max_iter 15 \
+        --max_attached_imgs 3 \
+        --temperature 1 \
+        --fix_box_color \
+        --api_model gpt-4.1-2025-04-14 \
+        --seed ${i} > logs/subset_batch_${i}.log &
+done
